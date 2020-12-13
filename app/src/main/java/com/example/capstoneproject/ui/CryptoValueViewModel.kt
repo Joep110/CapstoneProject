ec02623b9334ac8ad25f6919d1a9976aeead7d7b
joep <joepboekhold@gmail.com>
2020-12-13 17:54:47 +0100

    Added ViewModel

---
+++ app/src/main/java/com/example/capstoneproject/ui/CryptoValueViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.capstoneproject.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.capstoneproject.repository.CryptoValueRepository
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+
+class CryptoValueViewModel(application: Application): AndroidViewModel(application) {
+
+    private val cryptoValueRepository = CryptoValueRepository()
+
+    val cryptoValue =  cryptoValueRepository.cryptoValue
+
+    private val _errorText: MutableLiveData<String> = MutableLiveData()
+
+    val errorText: LiveData<String>
+        get() = _errorText
+
+    fun getPopularCryptoValues() {
+        viewModelScope.launch {
+            try {
+                cryptoValueRepository.getAllCurrencies()
+            } catch (error: CryptoValueRepository.CryptoValueRefreshError) {
+                _errorText.value = error.message
+            }
+        }
+    }
+}

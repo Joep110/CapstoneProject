a01362101df7a9887acfb3279dd204c0fc9189f1
joep <joepboekhold@gmail.com>
2020-12-27 19:49:17 +0100

    Added on item select on spinner

--- app/src/main/java/com/example/capstoneproject/ui/LatestCryptoFragment.kt
+++ app/src/main/java/com/example/capstoneproject/ui/LatestCryptoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -51,6 +52,18 @@ class LatestCryptoFragment : Fragment() {
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
+        }
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+            ) {
+                viewModel.getLatestCryptoValues()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
         }
     }
 
